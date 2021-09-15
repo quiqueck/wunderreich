8305dede95847213c8a9692bc738f25a10340d90
Frank <developer@ambertation.de>
2021-09-15 07:46:07 +0200

    Do not despawn name-tagged monsters in peacefull

---
+++ src/main/java/de/ambertation/wunderreich/mixin/MonsterMixin.java
@@ -0,0 +1,19 @@
+package de.ambertation.wunderreich.mixin;
+
+import net.minecraft.world.entity.monster.Monster;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Monster.class)
+public class MonsterMixin {
+    @Inject(method = "shouldDespawnInPeaceful", at=@At("HEAD"), cancellable = true)
+    protected void wunder_shouldDespawnInPeaceful(CallbackInfoReturnable<Boolean> cir) {
+        Monster m = (Monster)(Object)this;
+        if (m.hasCustomName()) {
+            cir.setReturnValue(false);
+            cir.cancel();
+        }
+    }
+}
