459047e2ed3a61bb7bd176761c5340748ae09436
Frank <developer@ambertation.de>
2022-01-17 01:31:17 +0100

    Som fixes to spreading code

--- src/main/java/de/ambertation/wunderreich/blocks/SpreadableSnowyDirtSlab.java
+++ src/main/java/de/ambertation/wunderreich/blocks/SpreadableSnowyDirtSlab.java
@@ -49,26 +49,40 @@ public class SpreadableSnowyDirtSlab extends SnowyDirtSlab {
     @Override
     public void randomTick(BlockState blockState, ServerLevel level, BlockPos blockPos, Random random) {
         if (!canBeGrass(blockState, level, blockPos)) {
-            level.setBlockAndUpdate(blockPos, WunderreichBlocks.DIRT_SLAB.defaultBlockState());
+            final BlockState testState = level.getBlockState(blockPos);
+            level.setBlockAndUpdate(
+                    blockPos,
+                    WunderreichBlocks.DIRT_SLAB
+                            .defaultBlockState()
+                            .setValue(WATERLOGGED, testState.getValue(WATERLOGGED))
+                            .setValue(TYPE, testState.getValue(TYPE))
+                                   );
             return;
         }
         if (level.getMaxLocalRawBrightness(blockPos.above()) >= 9) {
-            BlockState dirtSlabBlockState = this.defaultBlockState();
-            BlockState dirtBlockState = Blocks.DIRT.defaultBlockState();
+            final BlockState grassSlabBlockState = this.defaultBlockState();
+            final BlockState grassBlockState = Blocks.GRASS_BLOCK.defaultBlockState();
+            BlockState testState;
+
             for (int i = 0; i < 4; ++i) {
                 BlockPos testPos = blockPos.offset(random.nextInt(3) - 1,
                                                    random.nextInt(5) - 3,
                                                    random.nextInt(3) - 1);
-                if (!canPropagate(dirtSlabBlockState, level, testPos)) continue;
-
-                if (level.getBlockState(testPos).is(Blocks.DIRT)) {
-                    level.setBlockAndUpdate(testPos, dirtBlockState);
-                } else if (level.getBlockState(testPos).is(WunderreichBlocks.DIRT_SLAB)) {
+                if (!canPropagate(grassSlabBlockState, level, testPos)) continue;
+                testState = level.getBlockState(testPos);
+                if (testState.is(Blocks.DIRT)) {
                     level.setBlockAndUpdate(testPos,
-                                            dirtSlabBlockState.setValue(SNOWY,
-                                                                        level
-                                                                                .getBlockState(testPos.above())
-                                                                                .is(Blocks.SNOW)));
+                                            grassBlockState.setValue(SNOWY,
+                                                                     level.getBlockState(testPos.above())
+                                                                          .is(Blocks.SNOW)));
+                } else if (testState.is(WunderreichBlocks.DIRT_SLAB)) {
+                    final BlockState newState = grassSlabBlockState.setValue(SNOWY,
+                                                                             level.getBlockState(testPos.above())
+                                                                                  .is(Blocks.SNOW))
+                                                                   .setValue(WATERLOGGED,
+                                                                             testState.getValue(WATERLOGGED))
+                                                                   .setValue(TYPE, testState.getValue(TYPE));
+                    level.setBlockAndUpdate(testPos, newState);
                 }
             }
         }
