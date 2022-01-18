914bd4aa2c25488b47b2d087ab641b40cae5ca12
Frank <developer@ambertation.de>
2022-01-18 02:09:50 +0100

    Updated grass spreading code

--- src/main/java/de/ambertation/wunderreich/blocks/SpreadableSnowyDirtSlab.java
+++ src/main/java/de/ambertation/wunderreich/blocks/SpreadableSnowyDirtSlab.java
@@ -6,12 +6,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SnowLayerBlock;
+import net.minecraft.world.level.block.SpreadingSnowyDirtBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.lighting.LayerLightEngine;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import com.google.common.math.DoubleMath;
 
 import java.util.Random;
 
@@ -20,23 +26,72 @@ public class SpreadableSnowyDirtSlab extends SnowyDirtSlab {
         super(baseBlock);
     }
 
-    private static boolean canBeGrass(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
-        BlockPos blockPos2 = blockPos.above();
-        BlockState blockState2 = levelReader.getBlockState(blockPos2);
-        if (blockState2.is(Blocks.SNOW) && blockState2.getValue(SnowLayerBlock.LAYERS) == 1) {
-            return true;
-        }
-        if (blockState2.getFluidState().getAmount() == 8) {
+    public static boolean doesOcclude(BlockGetter blockGetter,
+                                      BlockState state,
+                                      BlockPos pos,
+                                      BlockState aboveState,
+                                      BlockPos abovePos,
+                                      Direction direction) {
+
+
+        if (!aboveState.canOcclude()) {
             return false;
         }
-        int i = LayerLightEngine.getLightBlockInto(levelReader,
-                                                   blockState,
-                                                   blockPos,
-                                                   blockState2,
-                                                   blockPos2,
-                                                   Direction.UP,
-                                                   blockState2.getLightBlock(levelReader, blockPos2));
-        return i < levelReader.getMaxLightLevel();
+
+        VoxelShape shape = state.getOcclusionShape(blockGetter, pos);
+        VoxelShape aboveShape = aboveState.getOcclusionShape(blockGetter, abovePos);
+        if (!aboveState.useShapeForLightOcclusion() && aboveShape != Shapes.block()) {
+            return false;
+        }
+
+        if (aboveShape.isEmpty()) return false;
+        Direction.Axis axis = direction.getAxis();
+        if (!DoubleMath.fuzzyEquals(shape.max(axis), 1.0, 1.0E-7)) {
+            return false;
+        }
+        if (!DoubleMath.fuzzyEquals(aboveShape.min(axis), 0.0, 1.0E-7)) {
+            return false;
+        }
+        return Shapes.mergedFaceOccludes(shape, aboveShape, direction);
+    }
+
+    private static boolean canBeGrassNewSlab(BlockState state,
+                                             LevelReader reader,
+                                             BlockPos pos) {
+        if (state.getValue(TYPE) == SlabType.BOTTOM) {
+            return true;
+        }
+
+        return canBeGrassNew(state, reader, pos);
+    }
+
+    private static boolean canBeGrassNew(BlockState state,
+                                         LevelReader reader,
+                                         BlockPos pos) {
+
+        BlockPos abovePos = pos.above();
+        BlockState aboveState = reader.getBlockState(abovePos);
+        if (state.is(Blocks.SNOW) && state.getValue(SnowLayerBlock.LAYERS) == 1) {
+            return true;
+        }
+        if (state.getFluidState().getAmount() == 8) {
+            return false;
+        }
+        return !doesOcclude(reader,
+                            state,
+                            pos,
+                            aboveState,
+                            abovePos,
+                            Direction.UP);
+    }
+
+    public static boolean canBeGrass(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
+        final Block bl = blockState.getBlock();
+        if (bl instanceof DirtSlabBlock) {
+            return canBeGrassNewSlab(blockState, levelReader, blockPos);
+        } else {
+            return SpreadingSnowyDirtBlock.canBeGrass(blockState, levelReader, blockPos);
+        }
     }
 
     private static boolean canPropagate(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
@@ -46,30 +101,42 @@ public class SpreadableSnowyDirtSlab extends SnowyDirtSlab {
                 .is(FluidTags.WATER);
     }
 
-    @Override
-    public void randomTick(BlockState blockState, ServerLevel level, BlockPos blockPos, Random random) {
+    public static void spreadingTick(Block me,
+                                     BlockState blockState,
+                                     ServerLevel level,
+                                     BlockPos blockPos,
+                                     Random random) {
         if (!canBeGrass(blockState, level, blockPos)) {
             final BlockState testState = level.getBlockState(blockPos);
-            level.setBlockAndUpdate(
-                    blockPos,
-                    WunderreichBlocks.DIRT_SLAB
-                            .defaultBlockState()
-                            .setValue(WATERLOGGED, testState.getValue(WATERLOGGED))
-                            .setValue(TYPE, testState.getValue(TYPE))
-                                   );
+            if (me instanceof SpreadableSnowyDirtSlab) {
+                level.setBlockAndUpdate(
+                        blockPos,
+                        WunderreichBlocks.DIRT_SLAB
+                                .defaultBlockState()
+                                .setValue(WATERLOGGED, testState.getValue(WATERLOGGED))
+                                .setValue(TYPE, testState.getValue(TYPE))
+                                       );
+            } else if (me instanceof SpreadingSnowyDirtBlock) {
+                level.setBlockAndUpdate(
+                        blockPos,
+                        Blocks.DIRT.defaultBlockState()
+                                       );
+            }
             return;
         }
+
         if (level.getMaxLocalRawBrightness(blockPos.above()) >= 9) {
-            final BlockState grassSlabBlockState = this.defaultBlockState();
+            final BlockState grassSlabBlockState = WunderreichBlocks.GRASS_SLAB.defaultBlockState();
             final BlockState grassBlockState = Blocks.GRASS_BLOCK.defaultBlockState();
             BlockState testState;
 
-            for (int i = 0; i < 4; ++i) {
+            for (int i = 0; i < 2; ++i) {
                 BlockPos testPos = blockPos.offset(random.nextInt(3) - 1,
                                                    random.nextInt(5) - 3,
                                                    random.nextInt(3) - 1);
-                if (!canPropagate(grassSlabBlockState, level, testPos)) continue;
                 testState = level.getBlockState(testPos);
+                if (!canPropagate(testState, level, testPos)) continue;
+
                 if (testState.is(Blocks.DIRT)) {
                     level.setBlockAndUpdate(testPos,
                                             grassBlockState.setValue(SNOWY,
@@ -86,5 +153,10 @@ public class SpreadableSnowyDirtSlab extends SnowyDirtSlab {
                 }
             }
         }
+    }
+
+    @Override
+    public void randomTick(BlockState blockState, ServerLevel level, BlockPos blockPos, Random random) {
+        spreadingTick(this, blockState, level, blockPos, random);
     }
 }
---
+++ src/main/java/de/ambertation/wunderreich/mixin/SpreadingSnowyDirtBlockMixin.java
@@ -0,0 +1,33 @@
+package de.ambertation.wunderreich.mixin;
+
+import de.ambertation.wunderreich.blocks.SpreadableSnowyDirtSlab;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.SpreadingSnowyDirtBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(SpreadingSnowyDirtBlock.class)
+public abstract class SpreadingSnowyDirtBlockMixin {
+
+
+    @Inject(method = "randomTick", at = @At("TAIL"))
+    void wunderreich_randomTick(BlockState blockState,
+                                ServerLevel serverLevel,
+                                BlockPos blockPos,
+                                Random random,
+                                CallbackInfo ci) {
+        SpreadableSnowyDirtSlab.spreadingTick((SpreadingSnowyDirtBlock) (Object) this,
+                                              blockState,
+                                              serverLevel,
+                                              blockPos,
+                                              random);
+    }
+}
