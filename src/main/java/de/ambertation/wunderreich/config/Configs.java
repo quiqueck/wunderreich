296d64a804d548ea1e02e35d5bce200a2a8a8f7a
Frank <developer@ambertation.de>
2022-03-28 12:01:26 +0200

    Removed TextConfig

--- src/main/java/de/ambertation/wunderreich/config/Configs.java
+++ src/main/java/de/ambertation/wunderreich/config/Configs.java
@@ -1,17 +1,6 @@
 package de.ambertation.wunderreich.config;
 
-class TestConfig extends ConfigFile {
-    public BooleanValue val1 = new BooleanValue("all.testA", "val1", false);
-    public BooleanValue val2 = new BooleanValue("all.testA", "val2", true).and(val1);
-    public IntValue val3 = new IntValue("ints", "val3", 42);
-
-    public TestConfig() {
-        super("test");
-    }
-}
-
 public class Configs {
-    public static final TestConfig TEST = new TestConfig();
     public static final MainConfig MAIN = new MainConfig();
     public static final BlockConfig BLOCK_CONFIG = new BlockConfig();
     public static final ItemConfig ITEM_CONFIG = new ItemConfig();
@@ -22,6 +11,5 @@ public class Configs {
         BLOCK_CONFIG.save();
         ITEM_CONFIG.save();
         RECIPE_CONFIG.save();
-        TEST.save();
     }
 }
