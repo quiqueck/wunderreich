0a0aac617d2b45ef82b5ad678d20ea1155f09143
Frank <developer@ambertation.de>
2022-04-03 11:24:45 +0200

    Disabled REI (no 1.19-Version yet)

--- src/main/java/de/ambertation/wunderreich/rei/ImprinterDisplay.java
+++ src/main/java/de/ambertation/wunderreich/rei/ImprinterDisplay.java
@@ -2,28 +2,28 @@ package de.ambertation.wunderreich.rei;
 
 import net.minecraft.world.item.crafting.Recipe;
 
-import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
-import me.shedaniel.rei.api.common.entry.EntryIngredient;
-import me.shedaniel.rei.api.common.util.EntryIngredients;
+//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+//import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+//import me.shedaniel.rei.api.common.entry.EntryIngredient;
+//import me.shedaniel.rei.api.common.util.EntryIngredients;
 
 import java.util.Collections;
 import java.util.List;
 
-public class ImprinterDisplay extends BasicDisplay {
-    public ImprinterDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
-        super(inputs, outputs);
-    }
-
-    public static ImprinterDisplay of(Recipe<?> recipe) {
-        return new ImprinterDisplay(
-                EntryIngredients.ofIngredients(recipe.getIngredients()),
-                Collections.singletonList(EntryIngredients.of(recipe.getResultItem()))
-        );
-    }
-
-    @Override
-    public CategoryIdentifier<?> getCategoryIdentifier() {
-        return ServerPlugin.IMPRINTER;
-    }
-}
+//public class ImprinterDisplay extends BasicDisplay {
+//    public ImprinterDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
+//        super(inputs, outputs);
+//    }
+//
+//    public static ImprinterDisplay of(Recipe<?> recipe) {
+//        return new ImprinterDisplay(
+//                EntryIngredients.ofIngredients(recipe.getIngredients()),
+//                Collections.singletonList(EntryIngredients.of(recipe.getResultItem()))
+//        );
+//    }
+//
+//    @Override
+//    public CategoryIdentifier<?> getCategoryIdentifier() {
+//        return ServerPlugin.IMPRINTER;
+//    }
+//}
--- src/main/java/de/ambertation/wunderreich/rei/ServerPlugin.java
+++ src/main/java/de/ambertation/wunderreich/rei/ServerPlugin.java
@@ -9,31 +9,31 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.item.ItemStack;
 
-import dev.architectury.utils.NbtType;
-import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
-import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
-import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
-
-import java.util.function.Function;
-
-public class ServerPlugin implements REIServerPlugin {
-    public static CategoryIdentifier<ImprinterDisplay> IMPRINTER = CategoryIdentifier.of(Wunderreich.MOD_ID,
-                                                                                         ImprinterRecipe.Type.ID.getPath());
-
-    @Override
-    public void registerItemComparators(ItemComparatorRegistry registry) {
-        EntryComparator<Tag> nbtHasher = EntryComparator.nbt();
-        Function<ItemStack, CompoundTag> enchantmentTag = stack -> {
-            CompoundTag tag = stack.getTag();
-            if (tag == null) return null;
-            if (tag.contains(TrainedVillagerWhisperer.TAG_NAME, NbtType.COMPOUND)) {
-                return tag.getCompound(TrainedVillagerWhisperer.TAG_NAME);
-            }
-            return new CompoundTag();
-        };
-
-        registry.register((context, stack) -> nbtHasher.hash(context, enchantmentTag.apply(stack)),
-                          WunderreichItems.WHISPERER);
-    }
-}
+//import dev.architectury.utils.NbtType;
+//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+//import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
+//import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
+//import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+//
+//import java.util.function.Function;
+//
+//public class ServerPlugin implements REIServerPlugin {
+//    public static CategoryIdentifier<ImprinterDisplay> IMPRINTER = CategoryIdentifier.of(Wunderreich.MOD_ID,
+//                                                                                         ImprinterRecipe.Type.ID.getPath());
+//
+//    @Override
+//    public void registerItemComparators(ItemComparatorRegistry registry) {
+//        EntryComparator<Tag> nbtHasher = EntryComparator.nbt();
+//        Function<ItemStack, CompoundTag> enchantmentTag = stack -> {
+//            CompoundTag tag = stack.getTag();
+//            if (tag == null) return null;
+//            if (tag.contains(TrainedVillagerWhisperer.TAG_NAME, NbtType.COMPOUND)) {
+//                return tag.getCompound(TrainedVillagerWhisperer.TAG_NAME);
+//            }
+//            return new CompoundTag();
+//        };
+//
+//        registry.register((context, stack) -> nbtHasher.hash(context, enchantmentTag.apply(stack)),
+//                          WunderreichItems.WHISPERER);
+//    }
+//}
