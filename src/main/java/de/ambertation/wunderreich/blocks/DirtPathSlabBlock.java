cd6efafe63730bd492cb882b53fa19ada8d97b0a
Frank <developer@ambertation.de>
2022-04-26 20:22:39 +0200

    Fixed tick Signature

--- src/main/java/de/ambertation/wunderreich/blocks/DirtPathSlabBlock.java
+++ src/main/java/de/ambertation/wunderreich/blocks/DirtPathSlabBlock.java
@@ -5,19 +5,20 @@ import de.ambertation.wunderreich.registries.WunderreichBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceGateBlock;
+import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-import java.util.Random;
-
-public class DirtPathSlabBlock extends DirtSlabBlock{
+public class DirtPathSlabBlock extends DirtSlabBlock {
     protected static final VoxelShape BOTTOM_AABB;
     protected static final VoxelShape TOP_AABB;
     protected static final VoxelShape DOUBLE_AABB;
@@ -32,12 +33,15 @@ public class DirtPathSlabBlock extends DirtSlabBlock{
     }
 
     @Override
-    public boolean isPathfindable(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, PathComputationType pathComputationType) {
+    public boolean isPathfindable(BlockState blockState,
+                                  BlockGetter blockGetter,
+                                  BlockPos blockPos,
+                                  PathComputationType pathComputationType) {
         return false;
     }
 
     @Override
-    public void tick(BlockState blockState, ServerLevel level, BlockPos blockPos, Random random) {
+    public void tick(BlockState blockState, ServerLevel level, BlockPos blockPos, RandomSource random) {
         final BlockState newState = DirtSlabBlock.createStateFrom(WunderreichBlocks.DIRT_SLAB, blockState);
         level.setBlockAndUpdate(blockPos, pushEntitiesUp(blockState, newState, level, blockPos));
     }
@@ -51,7 +55,12 @@ public class DirtPathSlabBlock extends DirtSlabBlock{
     }
 
     @Override
-    public BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState2, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos2) {
+    public BlockState updateShape(BlockState blockState,
+                                  Direction direction,
+                                  BlockState blockState2,
+                                  LevelAccessor levelAccessor,
+                                  BlockPos blockPos,
+                                  BlockPos blockPos2) {
         if (direction == Direction.UP && !blockState.canSurvive(levelAccessor, blockPos)) {
             levelAccessor.scheduleTick(blockPos, this, 1);
         }
@@ -59,9 +68,12 @@ public class DirtPathSlabBlock extends DirtSlabBlock{
     }
 
     @Override
-    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+    public VoxelShape getShape(BlockState blockState,
+                               BlockGetter blockGetter,
+                               BlockPos blockPos,
+                               CollisionContext collisionContext) {
         SlabType slabType = blockState.getValue(TYPE);
-        switch(slabType) {
+        switch (slabType) {
             case DOUBLE:
                 return DOUBLE_AABB;
             case TOP:
