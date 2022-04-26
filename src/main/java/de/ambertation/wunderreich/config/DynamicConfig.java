77c40820803de78218a64461b28afc5ee322cd8b
Frank <developer@ambertation.de>
2022-04-26 20:11:38 +0200

    Disabled Blocks will not get instanciated (#3).

--- src/main/java/de/ambertation/wunderreich/config/DynamicConfig.java
+++ src/main/java/de/ambertation/wunderreich/config/DynamicConfig.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.HashMap;
 
 public class DynamicConfig<T> extends ConfigFile {
+    private static final boolean DEFAULT_VALUE = true;
     private final HashMap<String, BooleanValue> dynamicConfig = new HashMap<>();
     private final HashMap<T, BooleanValue> dynamicLookup = new HashMap<>();
 
@@ -11,12 +12,19 @@ public class DynamicConfig<T> extends ConfigFile {
         super(category);
     }
 
+    public BooleanValue booleanOrDefault(String name) {
+        if (dynamicConfig.containsKey(name)) {
+            return dynamicConfig.get(name);
+        }
+        return new BooleanValue("", name, DEFAULT_VALUE);
+    }
+
     public BooleanValue newBooleanFor(String name, T object) {
         if (dynamicConfig.containsKey(name)) {
             return dynamicConfig.get(name);
         }
 
-        BooleanValue bv = new BooleanValue("", name, true);
+        BooleanValue bv = new BooleanValue("", name, DEFAULT_VALUE);
         dynamicConfig.put(name, bv);
         dynamicLookup.put(object, bv);
         return bv;
@@ -26,8 +34,11 @@ public class DynamicConfig<T> extends ConfigFile {
         return dynamicConfig.get(object);
     }
 
-    public BooleanValue valueOf(T object) {
-        return dynamicLookup.get(object);
+    public boolean valueOf(T object) {
+        if (object == null) return false;
+        BooleanValue bv = dynamicLookup.get(object);
+        if (bv == null) return false;
+        return bv.get();
     }
 
     public Collection<T> getAllObjects() {
