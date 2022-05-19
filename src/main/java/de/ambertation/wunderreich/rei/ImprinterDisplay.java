e07beb137f93dbe2d2d98e471241558e8142705a
Frank <developer@ambertation.de>
2022-05-19 18:02:28 +0200

    Re-Integrated REI

--- src/main/java/de/ambertation/wunderreich/rei/ImprinterDisplay.java
+++ src/main/java/de/ambertation/wunderreich/rei/ImprinterDisplay.java
@@ -1,19 +1,29 @@
 package de.ambertation.wunderreich.rei;
 
-//public class ImprinterDisplay extends BasicDisplay {
-//    public ImprinterDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
-//        super(inputs, outputs);
-//    }
-//
-//    public static ImprinterDisplay of(Recipe<?> recipe) {
-//        return new ImprinterDisplay(
-//                EntryIngredients.ofIngredients(recipe.getIngredients()),
-//                Collections.singletonList(EntryIngredients.of(recipe.getResultItem()))
-//        );
-//    }
-//
-//    @Override
-//    public CategoryIdentifier<?> getCategoryIdentifier() {
-//        return ServerPlugin.IMPRINTER;
-//    }
-//}
+import net.minecraft.world.item.crafting.Recipe;
+
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ImprinterDisplay extends BasicDisplay {
+    public ImprinterDisplay(List<EntryIngredient> inputs, List<EntryIngredient> outputs) {
+        super(inputs, outputs);
+    }
+
+    public static ImprinterDisplay of(Recipe<?> recipe) {
+        return new ImprinterDisplay(
+                EntryIngredients.ofIngredients(recipe.getIngredients()),
+                Collections.singletonList(EntryIngredients.of(recipe.getResultItem()))
+        );
+    }
+
+    @Override
+    public CategoryIdentifier<?> getCategoryIdentifier() {
+        return ServerPlugin.IMPRINTER;
+    }
+}
