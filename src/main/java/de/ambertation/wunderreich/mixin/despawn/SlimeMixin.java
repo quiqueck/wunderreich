1fd0347bee44257c8402ad4f87052c673a574934
Frank <developer@ambertation.de>
2022-06-09 23:19:29 +0200

    Moved all despawn related mixins to subfolder

--- src/main/java/de/ambertation/wunderreich/mixin/despawn/SlimeMixin.java
+++ src/main/java/de/ambertation/wunderreich/mixin/despawn/SlimeMixin.java
@@ -1,20 +1,20 @@
-package de.ambertation.wunderreich.mixin;
+package de.ambertation.wunderreich.mixin.despawn;
 
 import de.ambertation.wunderreich.registries.WunderreichRules;
 
-import net.minecraft.world.entity.monster.Phantom;
+import net.minecraft.world.entity.monster.Slime;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Phantom.class)
-public class PhantomMixin {
+@Mixin(Slime.class)
+public class SlimeMixin {
     @Inject(method = "shouldDespawnInPeaceful", at = @At("HEAD"), cancellable = true)
     protected void wunder_shouldDespawnInPeaceful(CallbackInfoReturnable<Boolean> cir) {
         if (WunderreichRules.doNotDespawnWithNameTag()) {
-            Phantom m = (Phantom) (Object) this;
+            Slime m = (Slime) (Object) this;
             if (m.hasCustomName()) {
                 cir.setReturnValue(false);
                 cir.cancel();
