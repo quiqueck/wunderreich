1a9611d15ed1392e75dcad95e2e7ed280849d480
Frank <developer@ambertation.de>
2022-06-29 09:38:33 +0200

    [fix] Enable drops for Walls and Stairs

--- src/main/java/de/ambertation/wunderreich/blocks/AbstractStairBlock.java
+++ src/main/java/de/ambertation/wunderreich/blocks/AbstractStairBlock.java
@@ -1,6 +1,7 @@
 package de.ambertation.wunderreich.blocks;
 
 import de.ambertation.wunderreich.interfaces.BlockTagSupplier;
+import de.ambertation.wunderreich.interfaces.CanDropLoot;
 
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
@@ -10,7 +11,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import java.util.function.Consumer;
 
-public class AbstractStairBlock extends net.minecraft.world.level.block.StairBlock implements BlockTagSupplier {
+public class AbstractStairBlock extends net.minecraft.world.level.block.StairBlock implements BlockTagSupplier, CanDropLoot {
     public AbstractStairBlock(Block baseBlock) {
         super(baseBlock.defaultBlockState(), BlockBehaviour.Properties.copy(baseBlock));
     }
--- src/main/java/de/ambertation/wunderreich/blocks/AbstractWallBlock.java
+++ src/main/java/de/ambertation/wunderreich/blocks/AbstractWallBlock.java
@@ -1,6 +1,7 @@
 package de.ambertation.wunderreich.blocks;
 
 import de.ambertation.wunderreich.interfaces.BlockTagSupplier;
+import de.ambertation.wunderreich.interfaces.CanDropLoot;
 
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
@@ -10,7 +11,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import java.util.function.Consumer;
 
-public class AbstractWallBlock extends net.minecraft.world.level.block.WallBlock implements BlockTagSupplier {
+public class AbstractWallBlock extends net.minecraft.world.level.block.WallBlock implements BlockTagSupplier, CanDropLoot {
 
     public AbstractWallBlock(Block baseBlock) {
         super(BlockBehaviour.Properties.copy(baseBlock));
