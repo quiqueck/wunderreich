13fd0f58e27d28008354b40cc847ce6fc350bf5b
Frank <developer@ambertation.de>
2022-10-13 22:01:51 +0200

    [Fix] Double slabs drop only a single item (#6)

--- src/main/java/de/ambertation/wunderreich/interfaces/CanDropLoot.java
+++ src/main/java/de/ambertation/wunderreich/interfaces/CanDropLoot.java
@@ -3,9 +3,15 @@ package de.ambertation.wunderreich.interfaces;
 import de.ambertation.wunderreich.loot.LootTableJsonBuilder;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
 
 public interface CanDropLoot {
     default LootTableJsonBuilder buildLootTable() {
+        if (this instanceof SlabBlock slab) {
+            return LootTableJsonBuilder
+                    .create(slab)
+                    .dropSelfSlab();
+        }
         if (this instanceof Block bl)
             return LootTableJsonBuilder.create(bl).dropSelf();
 
