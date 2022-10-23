d89668a003ea882584a20790a3291160a6be897d
Frank <developer@ambertation.de>
2022-10-23 22:48:08 +0200

    [Change] Disabled WTHIT integration for now

--- src/main/java/de/ambertation/wunderreich/integration/wthit/WailaPlugin.java
+++ src/main/java/de/ambertation/wunderreich/integration/wthit/WailaPlugin.java
@@ -1,13 +1,6 @@
 package de.ambertation.wunderreich.integration.wthit;
 
-import de.ambertation.wunderreich.Wunderreich;
-import de.ambertation.wunderreich.blocks.WunderKisteBlock;
-
-import mcp.mobius.waila.api.IRegistrar;
-import mcp.mobius.waila.api.IWailaPlugin;
-import mcp.mobius.waila.api.TooltipPosition;
-
-public class WailaPlugin implements IWailaPlugin {
+/*public class WailaPlugin implements IWailaPlugin {
     @Override
     public void register(IRegistrar registrar) {
         Wunderreich.LOGGER.info("Registering Waila-/Wthit-Plugin.");
@@ -16,4 +9,4 @@ public class WailaPlugin implements IWailaPlugin {
         registrar.addComponent(WunderkisteProvider.INSTANCE, TooltipPosition.BODY, WunderKisteBlock.class);
         registrar.addIcon(WunderkisteProvider.INSTANCE, WunderKisteBlock.class);
     }
-}
+}*/
