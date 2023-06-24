d4f535716ce748af23c36b9424b1d54807f5f858
Frank <developer@ambertation.de>
2023-06-24 20:59:05 +0200

    [Fix] Dynamic LootTables were not injected

--- src/main/java/de/ambertation/wunderreich/mixin/LootTablesMixin.java
+++ src/main/java/de/ambertation/wunderreich/mixin/LootTablesMixin.java
@@ -1,28 +1,31 @@
 package de.ambertation.wunderreich.mixin;
 
-import net.minecraft.resources.ResourceLocation;
+import de.ambertation.wunderreich.loot.LootTableJsonBuilder;
+
 import net.minecraft.world.level.storage.loot.LootDataManager;
 import net.minecraft.world.level.storage.loot.LootDataType;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.Map;
 
 
 @Mixin(value = LootDataManager.class, priority = 200)
 public class LootTablesMixin {
-    @Inject(method = "apply", at = @At("HEAD"))
-    public void wunderreich_interceptApply(
-            Map<LootDataType<?>, Map<ResourceLocation, ?>> map,
-            CallbackInfo info
-    ) {
-        System.out.println("WunderReich: LootTablesMixin.apply");
-//        LootTableJsonBuilder
-//                .getAllBlocks()
-//                .filter(e -> !map.containsKey(e.id()))
-//                .forEach(e -> map.put(e.id(), e.json().get()));
+    @ModifyArg(method = "scheduleElementParse", at = @At(value = "INVOKE", target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), index = 1)
+    private static <T> Object wunderreich_injectLootTables(Object key, Object value) {
+        if (value instanceof Map map) {
+            if (key instanceof LootDataType lootDataType && lootDataType == LootDataType.TABLE) {
+                LootTableJsonBuilder
+                        .getAllBlocks()
+                        .filter(e -> !map.containsKey(e.id()))
+                        .forEach(e -> lootDataType.deserialize(e.id(), e.json().get()).ifPresent((object) -> {
+                            map.put(e.id(), object);
+                        }));
+            }
+        }
+        return value;
     }
 }
