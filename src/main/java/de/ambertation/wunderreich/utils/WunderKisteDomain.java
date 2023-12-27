ec15a37207bcdb8d3ebee60d42c0d59dac93ba70
Frank <developer@ambertation.de>
2023-12-27 15:18:44 +0100

    [Feature] Support for Fabric Launcher 15.x

--- src/main/java/de/ambertation/wunderreich/utils/WunderKisteDomain.java
+++ src/main/java/de/ambertation/wunderreich/utils/WunderKisteDomain.java
@@ -48,7 +48,6 @@ public enum WunderKisteDomain implements StringRepresentable {
     public final int overlayColor;
     private final String name;
     public final boolean useMonochromeFallback;
-    @Environment(EnvType.CLIENT)
     private final Object texture;
 
     WunderKisteDomain(String name, Item triggerItem, int color, boolean useMonochromeFallback, String texture) {
