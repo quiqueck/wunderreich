f3cd5a877105f1e8e8e070633537291f94eda5cc
Frank <developer@ambertation.de>
2025-06-14 17:28:58 +0200

    Refactor LootTablesMixin to update method signature for loot table injection

--- src/main/java/de/ambertation/wunderreich/mixin/LootTablesMixin.java
+++ src/main/java/de/ambertation/wunderreich/mixin/LootTablesMixin.java
@@ -2,13 +2,16 @@ package de.ambertation.wunderreich.mixin;
 
 import de.ambertation.wunderreich.loot.LootTableJsonBuilder;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DynamicOps;
+import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.ReloadableServerRegistries;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.level.storage.loot.LootDataType;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,14 +22,16 @@ import java.util.Map;
 
 @Mixin(value = ReloadableServerRegistries.class, priority = 200)
 public class LootTablesMixin {
-    @ModifyArg(method = "method_58279", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/SimpleJsonResourceReloadListener;scanDirectory(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/lang/String;Lcom/google/gson/Gson;Ljava/util/Map;)V"))
+    @ModifyArg(method = "method_61240", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/resources/SimpleJsonResourceReloadListener;scanDirectory(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/resources/ResourceKey;Lcom/mojang/serialization/DynamicOps;Lcom/mojang/serialization/Codec;Ljava/util/Map;)V"))
     private static Map<ResourceLocation, JsonElement> wunderreich_injectLootTables(
             ResourceManager resourceManager,
-            String lootPath,
-            Gson gson,
+            ResourceKey<? extends Registry<JsonElement>> resourceKey,
+            DynamicOps<JsonElement> dynamicOps,
+            Codec<JsonElement> codec,
             Map<ResourceLocation, JsonElement> map
     ) {
         final String tablePath = Registries.elementsDirPath(LootDataType.TABLE.registryKey());
+        String lootPath = Registries.elementsDirPath(resourceKey);
         if (tablePath.equals(lootPath)) {
             LootTableJsonBuilder
                     .getAllBlocks()
