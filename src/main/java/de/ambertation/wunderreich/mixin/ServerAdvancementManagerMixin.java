cc3cb43680daecbdf42c198abe14a3b644a1cd7f
Frank <developer@ambertation.de>
2025-06-15 00:14:02 +0200

    Fixed crash due to Advencements

--- src/main/java/de/ambertation/wunderreich/mixin/ServerAdvancementManagerMixin.java
+++ src/main/java/de/ambertation/wunderreich/mixin/ServerAdvancementManagerMixin.java
@@ -2,13 +2,18 @@ package de.ambertation.wunderreich.mixin;
 
 import de.ambertation.wunderreich.registries.WunderreichAdvancements;
 
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.ServerAdvancementManager;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.util.profiling.ProfilerFiller;
 
-import com.google.gson.JsonElement;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,18 +22,25 @@ import java.util.Map;
 
 @Mixin(value = ServerAdvancementManager.class, priority = 200)
 public class ServerAdvancementManagerMixin {
+    @Shadow
+    @Final
+    private HolderLookup.Provider registries;
+
     @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"))
     public void wunder_interceptApply(
-            Map<ResourceLocation, JsonElement> map,
+            Map<ResourceLocation, Advancement> map,
             ResourceManager resourceManager,
             ProfilerFiller profiler,
             CallbackInfo info
     ) {
-
+        final var dynamicOps = registries.createSerializationContext(JsonOps.INSTANCE);
         WunderreichAdvancements.ADVANCEMENTS
                 .entrySet()
                 .stream()
                 .filter(e -> !map.containsKey(e.getKey()))
-                .forEach(e -> map.put(e.getKey(), e.getValue()));
+                .forEach(e -> {
+                    DataResult<Advancement> advancement = Advancement.CODEC.parse(dynamicOps, e.getValue());
+                    map.put(e.getKey(), advancement.getOrThrow());
+                });
     }
 }
