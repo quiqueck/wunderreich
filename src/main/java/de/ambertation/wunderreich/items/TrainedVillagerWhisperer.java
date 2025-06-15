b1254b8000d1181e209c3d7e309bc6119014b84f
Frank <developer@ambertation.de>
2025-06-15 02:49:34 +0200

    [Fix] Crash, when imprinter recipes have incomplete Data (fixes #26)

--- src/main/java/de/ambertation/wunderreich/items/TrainedVillagerWhisperer.java
+++ src/main/java/de/ambertation/wunderreich/items/TrainedVillagerWhisperer.java
@@ -44,6 +44,7 @@ public class TrainedVillagerWhisperer extends VillagerWhisperer {
 
     public static void addAllVariants(List<ItemStack> itemList) {
         ImprinterRecipe.getAllVariants()
+                       .filter(r -> r != null)
                        .forEach(r -> itemList.add(createForEnchantment(r.enchantment)));
     }
 
